f16b3f993bece36c5c82776e79bad5fbaeb97488
刘兆明 <paascloud.net@gmail.com>
2018-08-05 21:17:03 +0800

    提供支付宝沙箱账号, 增加支付体验

---
+++ src/views/order/payment/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="crumb">
+      <div class="w">
+        <div class="crumb-con">
+          <a class="link" href="javascript:void(0)" @click.prevent="goHome">PCMall</a>
+          <span>></span>
+          <span class="link-text">{{$route.meta.name}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="pay-wrap w">
+      <p class="payment-tips">订单提交成功，请尽快支付！订单号：{{orderNo}}</p>
+      <p class="payment-tips enhance">请使用支付宝沙箱App扫描如下二维码进行支付：</p>
+      <div class="img-con">
+        <img class="qr-code" v-lazy="optUploadFileRespDto.attachmentUrl" :key = "optUploadFileRespDto.attachmentUrl" alt="支付二维码"/>
+      </div>
+    </div>
+    <div class="pay-wrap w">
+      <p class="payment-tips">体验支付请扫描下方提供的二维码或者点击<a href="https://sandbox.alipaydev.com/user/downloadApp.htm">下载</a>沙箱APP</p>
+      <p class="payment-tips enhance">沙箱账号: fkdfrt3029@sandbox.com</p>
+      <p class="payment-tips enhance">登录密码: 111111 支付密码: 111111</p>
+      <div class="img-con">
+        <img class="qr-code" src="https://zos.alipayobjects.com/rmsportal/CaXHDDXkdxikcZP.png" alt="下载二维码"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        optUploadFileRespDto: {},
+        pollTimes: 0,
+        orderNo: '',
+        paymentTimer: null
+      };
+    },
+    activated() {
+      this.orderNo = this.$route.query.orderNo;
+      this.listenOrderStatus(this.orderNo);
+      this.queryOrderItemVoList(this.orderNo);
+    },
+    deactivated() {
+      clearInterval(this.paymentTimer);
+    },
+    methods: {
+      listenOrderStatus(orderNo) {
+        if (!orderNo) {
+          return;
+        }
+        let paymentTimer = setInterval(() => {
+          console.info('this.pollTimes', this.pollTimes);
+          this.pollTimes++;
+          if (this.pollTimes > 500) {
+            clearInterval(paymentTimer);
+            alert('支付超时');
+            this.loadPage('user-order');
+          }
+          this.ajax({
+            url: `/web/order/queryOrderPayStatus/` + orderNo,
+            isUnMusk: true,
+            success: (res) => {
+              if (res.result) {
+                alert('支付成功');
+                this.goHome();
+              }
+            }
+          });
+        }, 5e3);
+        this.paymentTimer = paymentTimer;
+      },
+      queryOrderItemVoList(orderNo) {
+        if (!orderNo) {
+          alert('订单号不存在');
+          return;
+        }
+        this.ajax({
+          url: `/web/pay/createQrCodeImage/` + orderNo,
+          success: (res) => {
+            if (res.code === 200) {
+              this.optUploadFileRespDto = res.result;
+            }
+          }
+        });
+      }
+    }
+  };
+</script>
+<style rel="stylesheet/scss" lang="scss">
+</style>
